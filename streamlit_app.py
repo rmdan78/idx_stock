cb2ff84860b245eb7c744a215e63df5075812b85
Muhammad ramdan <mhmhd.ramdan@yahoo.com>
2025-04-27 16:27:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,13 @@ if st.button('Predict'):
         prediction = pipeline.predict(input_data)
 
         # Menampilkan grafik garis harga penutupan 3 bulan terakhir
-        fig = px.line(stock_data, x=stock_data.index, y='Close', title=f'1 Year Close Prices for {ticker}')
+        fig = px.line(
+        stock_data,
+        x=stock_data.index,
+        y=stock_data['Close'].squeeze(),  # <-- tambahkan .squeeze()
+        title="Harga Penutupan Saham 3 Bulan Terakhir"
+        )
+
         st.plotly_chart(fig)
 
         # Menampilkan harga penutupan terbaru dalam format Rupiah
