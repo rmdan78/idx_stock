3fd6039ac1b01d8555eddb870cdcca584ad21e2c
Muhammad ramdan <mhmhd.ramdan@yahoo.com>
2025-04-27 16:51:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def calculate_features(data):
 st.title('Stock Price Prediction')
 st.info("This app uses a Neural Network-based model for stock price prediction. The model has been trained on historical stock data and includes technical features like moving averages and volatility.")
 st.header('Input Ticker Stock')
-ticker = st.text_input('Ticker', value='AAPL')
+ticker = st.text_input('Ticker', value='BBCA.JK')
 
 
 if st.button('Predict'):
